7d40d06e1dd754004b9c1f44444dec32974daad0
Nunchuk/ToasterOven <49164897+ToastedOven@users.noreply.github.com>
2023-01-02 20:56:57 -0500

    Initial commit 2 (also david doesn't use his LAPtop)

---
+++ PlateUpModTemplate/PlateUpMod.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using KitchenMods;
+
+namespace PlateUpModTemplate;
+
+public class PlateUpMod : IModInitializer
+{
+    public const string AUTHOR = "AUTHOR";
+    public const string MOD_NAME = "MOD_NAME";
+    public const string MOD_ID = $"com.{AUTHOR}.{MOD_NAME}";
+    
+    public void PostActivate(Mod mod)
+    {
+        Harmony.DEBUG = true;
+        Harmony harmony = new Harmony(MOD_ID);
+        
+        harmony.PatchAll();
+    }
+
+    public void PreInject()
+    {
+        
+    }
+
+    public void PostInject()
+    {
+        
+    }
+}
